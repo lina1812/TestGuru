17c8d684aebaca01e75393fc73ce8ee9919969f3
Alena Kudryavtseva-Dyakonova <alena.dyakonova999@gmail.com>
2021-04-01 23:28:11 +0300

    fix after review

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,44 +1,44 @@
 class QuestionsController < ApplicationController
-  
-  before_action :find_test
-  
+  before_action :find_test, only: %i[index create new]
+
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
-  
+
   def index
     render json: @test.questions.to_json
   end
-  
-  
+
   def show
-    @question = @test.questions.find(params[:id])
-    render json: @question.to_json
+    question = Question.find(params[:id])
+    render json: question.to_json
   end
-  
-  def new
-    
-  end
-  
-  def create  
+
+  def new; end
+
+  def create
     @question = @test.questions.create(question_params)
-    render  plain: @question.inspect
+    if @question.save
+      redirect_to test_questions_path
+    else
+      render :new
+    end
   end
-  
+
   def destroy
-    @question = @test.questions.find(params[:id])
-    @question.destroy
+    question = @test.questions.find(params[:id])
+    question.destroy
     render  plain: 'Question was deleted'
   end
-  
+
   private
-  
+
   def find_test
     @test = Test.find(params[:test_id])
   end
-  
+
   def question_params
     params.require(:question).permit(:body)
   end
-  
+
   def rescue_with_question_not_found
     render plain: 'Question was not found'
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-
   root 'tests#index'
-  
+
   resources :tests do
-    resources :questions
+    resources :questions, shallow: true
   end
-  
+
   get 'tests/:category/:title', to: 'tests#search'
 end
