fce3885f8dd3f9bb6dac531aa899299685758573
Alena Kudryavtseva-Dyakonova <alena.dyakonova999@gmail.com>
2021-04-03 21:24:30 +0300

    rubocop

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,9 +1,7 @@
 class Category < ApplicationRecord
-  
-  default_scope { order(:title) }  
-  
+  default_scope { order(:title) }
+
   has_many :tests, dependent: :restrict_with_exception
 
   validates :title, presence: true
-
 end
--- app/models/test.rb
+++ app/models/test.rb
@@ -10,9 +10,9 @@ class Test < ApplicationRecord
                     uniqueness: { scope: :level }
   validates :level, numericality: { only_integer: true, greater_than: 0 }
 
-  scope :easy, ->  { where(level: (0..1)) }
-  scope :medium, ->  { where(level: (2..4)) }
-  scope :difficult, ->  { where(level: (5..Float::INFINITY)) }
+  scope :easy, -> { where(level: (0..1)) }
+  scope :medium, -> { where(level: (2..4)) }
+  scope :difficult, -> { where(level: (5..Float::INFINITY)) }
 
   scope :by_category, ->(category_name) { joins(:category).where(categories: { title: category_name }) }
 
