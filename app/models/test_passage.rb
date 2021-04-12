3579fd7f1581b119f5fcb172765fcd109549cc27
Alena Kudryavtseva-Dyakonova <alena.dyakonova999@gmail.com>
2021-04-12 11:49:37 +0300

    add check for answer_ids

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -22,6 +22,7 @@ class TestPassage < ApplicationRecord
   private
 
   def correct_answer?(answer_ids)
+    return false if answer_ids.nil?
     correct_answers.ids.sort == answer_ids.map(&:to_i).sort
   end
 
