a63aa5b92df7650ca8dc4cc84b7f3c34a7ce52c3
Alena Kudryavtseva-Dyakonova <alena.dyakonova999@gmail.com>
2021-05-06 13:04:54 +0300

    add flash massage after sign_in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,16 @@
 class ApplicationController < ActionController::Base
+  
+  before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
   
   def after_sign_in_path_for(resource)
+     flash[:notice] = "Hello #{current_user.first_name} #{current_user.last_name}"
      current_user.is_a?(Admin) ? admin_tests_path : root_path
   end
+  
+  protected
 
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+    end
 end
