f86cb2f310126872064196f46d5a707dd9bbaa39
Alena Kudryavtseva-Dyakonova <alena.dyakonova999@gmail.com>
2021-05-17 15:40:51 +0300

    add locale to test from lesson

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,24 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_locale
 
   def after_sign_in_path_for(_resource)
     current_user.is_a?(Admin) ? admin_tests_path : root_path
+  end
+  
+  def default_url_options
+    { lang: I18n.locale }
   end
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name])
+  end
+
+  def set_locale
+    I18n.locale = I18n.locale_available?(params[:lang]) ? params[:lang] : I18n.default_locale
   end
 end
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,11 +1,9 @@
 # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
-
   # POST /resource/sign_in
   def create
     super
     flash[:notice] = "Hello #{current_user.first_name} #{current_user.last_name}"
   end
-
 end
