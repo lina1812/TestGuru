0052a8eb96c19fcd474c2ecf3d2bfa0c723e1705
Alena Kudryavtseva-Dyakonova <alena.dyakonova999@gmail.com>
2021-06-28 14:31:37 +0300

    add js for badges form

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,6 +15,7 @@ import "utilities/sorting_rows"
 import "utilities/form_inline"
 import "components/password_confirmation"
 import "components/progress_bar"
+import "components/hide_select"
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
